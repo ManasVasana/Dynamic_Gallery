4df860ceb9105837617990943a9e421f36a55160
ManasVasana <manasvasana@gmail.com>
2024-08-18 23:32:24 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Dynamic_Gallery/"
-})
+  plugins: [react(), ghPages()],
+  base: '/Dynamic_Gallery/', // Replace 'your-repo-name' with your actual repo name
+});
